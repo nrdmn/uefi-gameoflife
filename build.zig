9d5c94ff9f09860b392342703e9c96df361ca59d
Nick Erdmann <n@nirf.de>
2019-07-25 12:45:00 +0200

    adapt for uefi support in std library

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("bootx64", "gameoflife.zig");
+    exe.setBuildMode(b.standardReleaseOptions());
     exe.setTarget(builtin.Arch.x86_64, builtin.Os.uefi, builtin.Abi.none);
     exe.setOutputDir("EFI/Boot");
     b.default_step.dependOn(&exe.step);
--- gameoflife.zig
+++ gameoflife.zig
@@ -1,4 +1,4 @@
-const uefi = @import("zig-uefi/uefi.zig");
+const uefi = @import("std").os.uefi;
 
 const World = struct {
     buf: [24][80]bool,
@@ -56,8 +56,9 @@ const World = struct {
     }
 };
 
-export fn EfiMain(handle: uefi.types.Handle, system_table: *uefi.tables.SystemTable) noreturn {
-    _ = system_table.conOut().?.reset(false);
+pub fn main() noreturn {
+    const con_out = uefi.system_table.getConOut().?;
+    const stall = uefi.system_table.getBootServices().?.getStall().?;
     var world = World.init();
     world.buf[8][21] = true;
     world.buf[9][23] = true;
@@ -66,17 +67,19 @@ export fn EfiMain(handle: uefi.types.Handle, system_table: *uefi.tables.SystemTa
     world.buf[10][24] = true;
     world.buf[10][25] = true;
     world.buf[10][26] = true;
+    _ = con_out.reset(false);
     while (true) {
-        _ = system_table.conOut().?.setCursorPosition(0, 0);
+        _ = con_out.setCursorPosition(0, 0);
         for (world.buf) |row| {
             for (row) |c| {
                 if (c) {
-                    _ = system_table.conOut().?.outputString(&[_]uefi.types.Char16{ '#', 0 });
+                    _ = con_out.outputString(&[_]u16{ '#', 0 });
                 } else {
-                    _ = system_table.conOut().?.outputString(&[_]uefi.types.Char16{ ' ', 0 });
+                    _ = con_out.outputString(&[_]u16{ ' ', 0 });
                 }
             }
         }
         world.step();
+        _ = stall(100000);
     }
 }
