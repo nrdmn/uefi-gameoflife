58bfe0fc28b201fd6cdda9006c30ee3032b12e90
Nick Erdmann <n@nirf.de>
2019-07-26 18:02:22 +0200

    use new target syntax

--- build.zig
+++ build.zig
@@ -1,10 +1,18 @@
 const Builder = @import("std").build.Builder;
+const Target = @import("std").build.Target;
+const CrossTarget = @import("std").build.CrossTarget;
 const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("bootx64", "gameoflife.zig");
     exe.setBuildMode(b.standardReleaseOptions());
-    exe.setTarget(builtin.Arch.x86_64, builtin.Os.uefi, builtin.Abi.none);
+    exe.setTheTarget(Target{
+        .Cross = CrossTarget{
+            .arch = builtin.Arch.x86_64,
+            .os = builtin.Os.uefi,
+            .abi = builtin.Abi.none,
+        },
+    });
     exe.setOutputDir("EFI/Boot");
     b.default_step.dependOn(&exe.step);
 }
